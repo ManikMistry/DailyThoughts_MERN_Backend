4a5f8abc15a5736cc5063d14145673d48f6a39c1
Manik Mistry <mkmistryhd2019@gmail.com>
2024-06-03 16:25:22 +0530

    changes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const connectDB = async () => {
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: BASE_URL, credentials: true }));
+app.use(cors({origin: '*'}));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
